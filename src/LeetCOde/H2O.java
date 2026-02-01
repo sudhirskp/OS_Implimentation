dac03ff64fe702f03e5da729971330af5bebb234
sudhirskp <gnitwallah@gmail.com>
2026-02-01 11:21:47 +0530

    add semaphore-based implementation for hydrogen and oxygen synchronization in H2O class

--- src/LeetCOde/H2O.java
+++ src/LeetCOde/H2O.java
@@ -1,9 +1,10 @@
 package LeetCOde;
 
 import javax.swing.text.html.HTMLWriter;
+import java.util.concurrent.Semaphore;
 
 public class H2O {
-
+/*
     private int count;
     Object lock;
     public H2O() {
@@ -35,6 +36,28 @@ public class H2O {
             count = 0;
             lock.notifyAll();
         }
+    }
+    */
+
+    Semaphore hydrogen, oxygen;
+
+    public H2O() {
+        hydrogen = new Semaphore(2);
+        oxygen = new Semaphore(0);
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        hydrogen.acquire();
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+        oxygen.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        oxygen.acquire(2);
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+        releaseOxygen.run();
+        hydrogen.release(2);
     }
 
     public static void main(String[] args) {
